441e68c4ad9ef0ca8ce2fe24ef17e0468b90225d
juanvallejo <juan.vallejo.12@cnu.edu>
2014-04-08 16:54:31 -0400

    changing db to localhost

--- css/lib/config.php
+++ css/lib/config.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysql_connect('127.0.0.1','spujet','metamorph2016') or die(mysql_error());
+$link = mysql_connect('localhost','spujet','metamorph2016') or die(mysql_error());
 if(mysql_select_db('spujet_jjv',$link)) return true;
 else {die('There was an error connecting to the database');}
 ?>
