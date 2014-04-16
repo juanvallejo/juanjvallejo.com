b3ae2cff654aca6f3f9ce8b5d120871e20d81f05
juanvallejo <juan.vallejo.12@cnu.edu>
2014-04-16 03:55:56 -0400

    added new files

---
+++ files/stemday_web/server.js
@@ -0,0 +1,32 @@
+var fs = require('fs'),server = require('http').createServer(server).listen(8000);
+
+function server(req,res) {
+		var Type = {
+			'css':'text/css',
+			'html':'text/html',
+			'jpg':'image/jpeg',
+			'jpeg':'image/jpeg',
+			'js':'application/javascript',
+			'map':'application/x-navimap',
+			'pdf':'application/pdf',
+			'png':'image/png',
+			'txt':'text/plain'
+		};
+
+		var path = req.url == '/' ? 'index.html' : req.url;
+		var file = path.split("/");
+		var ext = 'txt';
+		
+		file = file[file.length-1];
+		ext = file.split(".");
+		ext = ext[ext.length-1];
+
+		fs.readFile(__dirname+'/'+path,function(err,data) {
+			if(err) {
+				res.writeHead(500);
+				return res.end("The file '"+path+"' could not be found.");
+			}
+			res.writeHead(200,{'Content-type':(Type[ext] || Type["text"])});
+			res.end(data);
+		})
+}
